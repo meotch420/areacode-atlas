1296ee75b2f336740f3d2eeef545f831421cea53
Mitch Weisberger <mweisberger420@gmail.com>
2026-03-22 23:39:08 +0200

    Update areacode.js

--- areacode.js
+++ areacode.js
@@ -1,5 +1,5 @@
 const DATA_FILE = "area_code_data.json";
-const MAPTILER_KEY = "x9Y4l8HNTbDkaAGphr9L";
+const MAPTILER_KEY = "TRZg1QKiYa41B03OE9Bz";
 
 const worldBounds = L.latLngBounds(
   [-85, -180],
