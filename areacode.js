2064ad52804eb45c2ba076797efc4d05b52d06d7
Mitch Weisberger <mweisberger420@gmail.com>
2026-04-06 15:02:42 +0300

    Update areacode.js

--- areacode.js
+++ areacode.js
@@ -404,7 +404,7 @@ window.addEventListener("DOMContentLoaded", () => {
       paint: {
         "fill-color": [
           "match",
-          ["get", "__tz"],
+          ["get", "__name"],
           "Eastern", getTimezoneColor("Eastern"),
           "Central", getTimezoneColor("Central"),
           "Mountain", getTimezoneColor("Mountain"),
