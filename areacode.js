94ce2435fdfae0a8ef644f2304baa0928c6dbccb
Mitch Weisberger <mweisberger420@gmail.com>
2026-05-18 20:10:14 +0300

    Zoom farther out on initial flat world map view

--- areacode.js
+++ areacode.js
@@ -33,7 +33,7 @@
     name: "United States",
     countryCode: "+1",
     center: [0, 15],
-    zoom: 1.3
+    zoom: 0.75
   };
   const AREA_CODE_SEARCH_ZOOM = 4.2;
 
@@ -95,7 +95,7 @@
       style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${MAPTILER_KEY}`,
       center: DEFAULT_COUNTRY_VIEW.center,
       zoom: DEFAULT_COUNTRY_VIEW.zoom,
-      minZoom: 0.5,
+      minZoom: 0,
       maxZoom: 18,
       projection: "mercator",
       renderWorldCopies: false,
@@ -167,7 +167,7 @@
     map.fitBounds(
       [[-180, -85], [180, 85]],
       {
-        padding: 8,
+        padding: 0,
         duration: 0
       }
     );
